acdc08f669faa98c4ee03af9cc9e762d2d878b45
shiva <shivanaik7924@gmail.com>
2025-08-03 11:53:03 +0530

    Update project files

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -4,9 +4,9 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host: 'redis-19934.c212.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 19934
-    }
+       host: 'redis-16334.c278.us-east-1-4.ec2.redns.redis-cloud.com',
+        port: 16334 
+    }   
 });
 
 module.exports = redisClient;
